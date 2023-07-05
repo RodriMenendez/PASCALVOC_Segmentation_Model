068790e083e4772a8e5a7dc0f5d8267e1d9fb4c9
Angel Avelar <rodri@Angels-MacBook-Air.local>
2023-07-05 14:29:49 -0700

    added another conv layer to segmentation model

--- models.py
+++ models.py
@@ -12,19 +12,26 @@ class Segmentation(nn.Module):
         self.out_channels = out_channels
         self.m = torchvision.ops.FeaturePyramidNetwork(in_channels_list, out_channels)
 
+        # conv layer 1
+        self.conv1 = nn.Conv2d(self.out_channels, self.out_channels//2, kernel_size=(3, 3), padding=1)
+        self.conv2 = nn.Conv2d(self.out_channels, self.out_channels//2, kernel_size=(3, 3), padding=1)
+        self.conv3 = nn.Conv2d(self.out_channels, self.out_channels//2, kernel_size=(3, 3), padding=1)
+        self.conv4 = nn.Conv2d(self.out_channels, self.out_channels//2, kernel_size=(3, 3), padding=1)
+        self.bn1 = nn.BatchNorm2d(self.out_channels//2)
+        self.relu = nn.ReLU()
+
+        # regularization
+        self.dropout = nn.Dropout(0.2)
+
         # upsampling
         self.upsample_4 = nn.Upsample(scale_factor=4, mode='nearest')
         self.upsample_8 = nn.Upsample(scale_factor=8, mode='nearest')
         self.upsample_16 = nn.Upsample(scale_factor=16, mode='nearest')
         self.upsample_32 = nn.Upsample(scale_factor=32, mode='nearest')
 
-        # regularization
-        self.dropout = nn.Dropout(0.2)
-
-        # conv
-        self.conv = nn.Conv2d(self.out_channels*4, 22, kernel_size=(3, 3), padding=1)
-        self.bn = nn.BatchNorm2d(22)
-        self.relu = nn.ReLU()
+        # conv output layer
+        self.conv_out = nn.Conv2d((self.out_channels//2)*4, 22, kernel_size=(3, 3), padding=1)
+        self.bn2 = nn.BatchNorm2d(22)
 
 
     def resnet_tail(self, input):
@@ -52,15 +59,20 @@ class Segmentation(nn.Module):
     def logits(self, input):
         fpn_output = self.FPN(input)
 
+        ly1_cnn = self.dropout(self.relu(self.bn1(self.conv1(fpn_output['layer1']))))
+        ly2_cnn = self.dropout(self.relu(self.bn1(self.conv2(fpn_output['layer2']))))
+        ly3_cnn = self.dropout(self.relu(self.bn1(self.conv3(fpn_output['layer3']))))
+        ly4_cnn = self.dropout(self.relu(self.bn1(self.conv4(fpn_output['layer4']))))
+
         # make all outputs of fpn have the same height and width
-        ly1_upsample = self.upsample_4(fpn_output['layer1'])
-        ly2_upsample = self.upsample_8(fpn_output['layer2'])
-        ly3_upsample = self.upsample_16(fpn_output['layer3'])
-        ly4_upsample = self.upsample_32(fpn_output['layer4'])
+        ly1_upsample = self.upsample_4(ly1_cnn)
+        ly2_upsample = self.upsample_8(ly2_cnn)
+        ly3_upsample = self.upsample_16(ly3_cnn)
+        ly4_upsample = self.upsample_32(ly4_cnn)
 
         # concatenate into one tensor and use cnn to get correct number of channels
         output = torch.cat((ly1_upsample, ly2_upsample, ly3_upsample, ly4_upsample), 1)
-        output = self.bn(self.conv(self.dropout(output)))
+        output = self.bn2(self.conv_out(output))
 
         return output
 
