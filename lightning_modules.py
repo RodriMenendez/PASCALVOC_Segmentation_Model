8943b16754a50096642d4d647974210f74c0f477
Angel Avelar <rodri@Angels-MacBook-Air.local>
2023-07-07 06:14:20 -0700

    saving model output at different steps to reduce computation

--- lightning_modules.py
+++ lightning_modules.py
@@ -4,11 +4,15 @@ import torch.nn.functional as F
 import lightning as L
 import evaluate
 
-class PASCALModule(L.LightningModule):
+class PascalModule(L.LightningModule):
     def __init__(self, model, lr=1e-3, momentum=0.9, ignore_index=0):
         super().__init__()
         self.model = model
-        self.loss_module = nn.CrossEntropyLoss()
+        weights = torch.tensor([0.5, 1, 1, 1, 1, 1, 1,\
+                                1, 1, 1, 1, 1, 1, 1,\
+                                1, 1, 1, 1, 1, 1, 1, 1])
+        self.seg_loss_module = nn.CrossEntropyLoss()
+        self.clf_loss_module = nn.CrossEntropyLoss(weight=weights)
         self.lr = lr
         self.momentum = momentum
         self.iou = evaluate.load("mean_iou")
@@ -21,17 +25,63 @@ class PASCALModule(L.LightningModule):
         optimizer = torch.optim.SGD(self.model.parameters(), lr=self.lr, momentum=self.momentum)
         return optimizer
 
+    def labels_clf_one_hot(self, labels):
+        u = None
+        for label in labels:
+            loh = torch.sum(F.one_hot(torch.unique(label.to(torch.int64)), num_classes=22), dim=0)
+            if torch.is_tensor(u):
+                try:
+                    u = torch.stack((u, loh), dim=0)
+                except:
+                    u = torch.cat((u, loh.unsqueeze(0)))
+            else:
+                u = loh
+
+        return u
+
+    def model_classes(self, labels_clf_one_hot):
+        input = labels_clf_one_hot
+        input = (input==1).nonzero()
+
+        cat_dict = {}
+
+        for pair in input:
+            pair = pair.numpy()
+            index, value = pair[0], pair[1]
+            if index in cat_dict:
+                cat_dict[index].append(value)
+            else:
+                cat_dict[index] = [value]
+
+        return cat_dict
+
     def training_step(self, batch, batch_idx):
         inputs, labels = batch
 
+        # one hot encoding on labels
         labels_one_hot = torch.permute(F.one_hot(labels.to(torch.int64), num_classes=22), (0, 3, 1, 2))
+        labels_clf_one_hot = self.labels_clf_one_hot(labels)
 
+        if len(labels_clf_one_hot.shape) == 1:
+            # batch dimension not on labels if only input
+            labels_clf_one_hot = labels_clf_one_hot.unsqueeze(0)
+
+        # logits for segmentation and classification
         logits = self.model.logits(inputs)
-        loss = self.loss_module(logits, labels_one_hot.float())
+        logits_clf = self.model.classification_logits(inputs)
+
+        # calcuating loss
+        loss_seg = self.seg_loss_module(logits, labels_one_hot.float())
+        loss_clf = self.clf_loss_module(logits_clf, labels_clf_one_hot.float())
+        loss = torch.add(loss_seg, loss_clf)
+
+        # calculating sementation metrics
         preds = self.model(inputs)
         iou = self.iou.compute(predictions=preds, references=labels, num_labels=22, ignore_index=self.ignore_index)
         mean_iou = iou['mean_iou']
         acc = iou['mean_accuracy']
+
+        # calculating classification metrics
 
         # log accuracy and loss
         self.log("Train Pixel Accuracy", acc)
@@ -39,6 +89,20 @@ class PASCALModule(L.LightningModule):
         self.log("Train Loss", loss)
 
         return loss
+
+    def validation_step(self, batch, batch_idx):
+        inputs, labels = batch
+        preds = self.model(inputs)
+
+        logits_clf = self.model.classification_logits(inputs)
+        iou = self.iou.compute(predictions=preds, references=labels, num_labels=22, ignore_index=self.ignore_index)
+        mean_iou = iou['mean_iou']
+        acc = iou['mean_accuracy']
+
+        # log accuracy and loss
+        self.log("Validation Pixel Accuracy", acc)
+        self.log("Validation Mean IoU", mean_iou)
+
 
     def test_step(self, batch, batch_idx):
         inputs, labels = batch
@@ -48,4 +112,4 @@ class PASCALModule(L.LightningModule):
         acc = iou['mean_accuracy']
 
         self.log("Test Pixel Accuracy", acc)
-        self.log("Tets Mean IoU", mean_iou)
+        self.log("Test Mean IoU", mean_iou)
--- models.py
+++ models.py
@@ -12,33 +12,34 @@ class Segmentation(nn.Module):
         self.out_channels = out_channels
         self.m = torchvision.ops.FeaturePyramidNetwork(in_channels_list, out_channels)
 
-        # conv layer 1
-        self.conv1 = nn.Conv2d(self.out_channels, self.out_channels//2, kernel_size=(3, 3), padding=1)
-        self.conv2 = nn.Conv2d(self.out_channels, self.out_channels//2, kernel_size=(3, 3), padding=1)
-        self.conv3 = nn.Conv2d(self.out_channels, self.out_channels//2, kernel_size=(3, 3), padding=1)
-        self.conv4 = nn.Conv2d(self.out_channels, self.out_channels//2, kernel_size=(3, 3), padding=1)
-        self.bn1 = nn.BatchNorm2d(self.out_channels//2)
-        self.relu = nn.ReLU()
-
-        # regularization
-        self.dropout = nn.Dropout(0.2)
-
         # upsampling
         self.upsample_4 = nn.Upsample(scale_factor=4, mode='nearest')
         self.upsample_8 = nn.Upsample(scale_factor=8, mode='nearest')
         self.upsample_16 = nn.Upsample(scale_factor=16, mode='nearest')
         self.upsample_32 = nn.Upsample(scale_factor=32, mode='nearest')
 
-        # conv output layer
-        self.conv_out = nn.Conv2d((self.out_channels//2)*4, 22, kernel_size=(3, 3), padding=1)
-        self.bn2 = nn.BatchNorm2d(22)
+        # regularization
+        self.dropout = nn.Dropout(0.2)
+
+        # conv
+        self.conv = nn.Conv2d(self.out_channels*4, 22, kernel_size=(3, 3), padding=1)
+        self.bn = nn.BatchNorm2d(22)
+        self.relu = nn.ReLU()
+
+        # classification
+        self.l1 = nn.Linear(1000, 1000)
+        self.l2 = nn.Linear(1000, 22)
+
+        # variables that are useful to store in the model
+        self.resnet_cnn = None
 
 
     def resnet_tail(self, input):
         """
         Goes through the first layers of the resnet model
         """
-        return self.resnet.maxpool(self.resnet.relu(self.resnet.bn1(self.resnet.conv1(input))))
+        output = self.resnet.maxpool(self.resnet.relu(self.resnet.bn1(self.resnet.conv1(input))))
+        return output
 
     def FPN(self, input):
         input = self.resnet_tail(input)
@@ -47,6 +48,8 @@ class Segmentation(nn.Module):
         ly2_out = self.resnet.layer2(ly1_out)
         ly3_out = self.resnet.layer3(ly2_out)
         ly4_out = self.resnet.layer4(ly3_out)
+
+        self.resnet_cnn = ly4_out
 
         x = OrderedDict()
         x['layer1'] = ly1_out
@@ -59,26 +62,37 @@ class Segmentation(nn.Module):
     def logits(self, input):
         fpn_output = self.FPN(input)
 
-        ly1_cnn = self.dropout(self.relu(self.bn1(self.conv1(fpn_output['layer1']))))
-        ly2_cnn = self.dropout(self.relu(self.bn1(self.conv2(fpn_output['layer2']))))
-        ly3_cnn = self.dropout(self.relu(self.bn1(self.conv3(fpn_output['layer3']))))
-        ly4_cnn = self.dropout(self.relu(self.bn1(self.conv4(fpn_output['layer4']))))
-
         # make all outputs of fpn have the same height and width
-        ly1_upsample = self.upsample_4(ly1_cnn)
-        ly2_upsample = self.upsample_8(ly2_cnn)
-        ly3_upsample = self.upsample_16(ly3_cnn)
-        ly4_upsample = self.upsample_32(ly4_cnn)
+        ly1_upsample = self.upsample_4(fpn_output['layer1'])
+        ly2_upsample = self.upsample_8(fpn_output['layer2'])
+        ly3_upsample = self.upsample_16(fpn_output['layer3'])
+        ly4_upsample = self.upsample_32(fpn_output['layer4'])
 
         # concatenate into one tensor and use cnn to get correct number of channels
         output = torch.cat((ly1_upsample, ly2_upsample, ly3_upsample, ly4_upsample), 1)
-        output = self.bn2(self.conv_out(output))
+        output = self.bn(self.conv(self.dropout(output)))
+
+        return output
+
+    def saved_classification_logits(self):
+        # already calculated resnet model output up to layer 4
+        output = self.resnet.avgpool(self.resnet_cnn).squeeze(-1).squeeze(-1)
+        output = self.resnet.fc(output)
+        output = F.relu(self.l1(output))
+        output = self.l2(output)
 
         return output
 
     def forward(self, input):
         logits = self.logits(input)
         return torch.argmax(logits, dim = 1)
+
+    def classification_logits(self, input):
+        output = self.resnet(input)
+        output = F.relu(self.l1(output))
+        output = self.l2(output)
+
+        return output
 
 def ResNet18():
     return torchvision.models.resnet18(weights='DEFAULT')
